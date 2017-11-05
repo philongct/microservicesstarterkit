969dd033553f4a4b2f246b5a984571281e8b6e53
U-Long-PC\Long <nhphilong@gmail.com>
2017-11-05 23:44:03 +0700

    Add basic components for microservices

---
+++ fwk/api-gateway/src/main/java/l/nguyen/fwk/gw/ApiGateway.java
@@ -0,0 +1,29 @@
+package l.nguyen.fwk.gw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
+
+/*
+   Copyright 2015 kennybastani, http://www.kennybastani.com/2015/07/spring-cloud-docker-microservices.html
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+@SpringBootApplication
+@EnableSidecar
+public class ApiGateway {
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateway.class, args);
+	}
+}
---
+++ fwk/config-server/src/main/java/l/nguyen/fwk/cfg/ConfigServer.java
@@ -0,0 +1,24 @@
+package l.nguyen.fwk.cfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+/**
+ * The {@link EnableConfigServer} annotation defines that this application will
+ * serve as the REST based API for providing external configuration. <br>
+ * <br>
+ * 
+ * The external repository from where the configuration will be picked up is
+ * defined in the {@linkplain application.yml} file.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServer {
+	public static void main(String[] args) {
+		// force using native config
+		System.setProperty("spring.profiles.active", "native");
+		SpringApplication.run(ConfigServer.class, args);
+	}
+}
