bb58ff1e20984efa9622e8028f11e807f8bec7ec
U-Long-PC\Long <nhphilong@gmail.com>
2017-11-15 21:33:33 +0700

    Add configure OAuth2 interceptor for Feign client

--- microservices/transaction-service/src/main/java/l/nguyen/ms/transaction/CreditCardTransactionApplication.java
+++ microservices/transaction-service/src/main/java/l/nguyen/ms/transaction/CreditCardTransactionApplication.java
@@ -1,16 +1,15 @@
 package l.nguyen.ms.transaction;
 
-import l.nguyen.security.config.oath2.OAuth2ResourceServerConfig;
+import l.nguyen.security.config.oath2.FeignOAuth2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-public class CreditCardTransactionApplication {
+public class CreditCardTransactionApplication extends FeignOAuth2Config {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CreditCardTransactionApplication.class, args);
---
+++ microservices/transaction-service/src/main/java/l/nguyen/ms/transaction/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package l.nguyen.ms.transaction.config;
+
+import l.nguyen.security.config.basicweb.AbstractSecurityConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+public class SecurityConfig extends AbstractSecurityConfigurer {
+
+    @Override
+    protected void configureAuthRequests(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .httpBasic().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .permitAll();
+    }
+}
