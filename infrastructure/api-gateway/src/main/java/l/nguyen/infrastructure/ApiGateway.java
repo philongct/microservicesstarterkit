b310ee636a2561718d1806606740e2bc2b03d079
U-Long-PC\Long <nhphilong@gmail.com>
2017-11-17 23:34:21 +0700

    Add OAuth2 SSO to Gateway (Zuul proxy)

--- infrastructure/api-gateway/src/main/java/l/nguyen/infrastructure/ApiGateway.java
+++ infrastructure/api-gateway/src/main/java/l/nguyen/infrastructure/ApiGateway.java
@@ -1,8 +1,14 @@
 package l.nguyen.infrastructure;
 
+import l.nguyen.security.config.oath2.OAuth2ResourceServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.sidecar.EnableSidecar;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 /*
    Copyright 2015 kennybastani, http://www.kennybastani.com/2015/07/spring-cloud-docker-microservices.html
@@ -22,7 +28,21 @@ import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 
 @SpringBootApplication
 @EnableSidecar
-public class ApiGateway {
+@EnableOAuth2Sso
+public class ApiGateway extends OAuth2ResourceServerConfig {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http
+            .authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+            .and()
+                .csrf().disable()
+                .httpBasic().disable();
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGateway.class, args);
 	}
