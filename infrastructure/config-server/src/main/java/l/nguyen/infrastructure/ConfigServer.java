2ff722c76dea839899c6a810a09c03d369cc8e44
U-Long-PC\Long <nhphilong@gmail.com>
2017-11-19 09:55:32 +0700

    Remove https in Config Server

--- infrastructure/config-server/src/main/java/l/nguyen/infrastructure/ConfigServer.java
+++ infrastructure/config-server/src/main/java/l/nguyen/infrastructure/ConfigServer.java
@@ -1,10 +1,8 @@
 package l.nguyen.infrastructure;
 
-import l.nguyen.security.config.basicweb.AbstractSecurityConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 
 
 /**
@@ -13,18 +11,11 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
  * <br>
  * 
  * The external repository from where the configuration will be picked up is
- * defined in the {application.yml} file.
+ * defined in the {@linkplain application.yml} file.
  */
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServer extends AbstractSecurityConfigurer {
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        // we don't want security now
-        web.ignoring().antMatchers("/**");
-    }
-
+public class ConfigServer {
 	public static void main(String[] args) {
 		// force using native config
 		System.setProperty("spring.profiles.active", "native");
